6d1d6a42cd12c3d657b0315ed86fa4a86b950b5e
Helmer Salguero <helmersalg@gmail.com>
2018-11-06 10:41:10 -0500

    buttons

---
+++ helloworld.js
@@ -0,0 +1,17 @@
+
+function sayHelloConsole () {
+  console.log("Hello, World!")
+}
+function sayHelloPopup () {
+  alert("Hello, World!")
+}
+
+function sayHelloBrowser() {
+  var p=document.getElementById("hello");
+  p.innerHTML="Hello, World!";
+}
+
+function sayHelloStranger() {
+  var i=prompt("Please enter your name", "Helmer")
+  alert("Hello" + i)
+}
